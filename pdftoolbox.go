18a7acadbb8b2c8265b3402130a8d080d0e5cd45
Byron Williams <byron@fika.studio>
2024-11-04 08:35:11 +0100

    feat: parse enumerateprofiles response

--- pdftoolbox.go
+++ pdftoolbox.go
@@ -1,8 +1,12 @@
 package pdftoolbox
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
+	"os"
+	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -70,4 +74,131 @@ func (cl *Client) RunProfile(profile string, inputFiles []string, args ...Arg) (
 	return &Result{
 		Command: fullCommand,
 	}, nil
+}
+
+func (cl *Client) EnumerateProfiles(profileFolder string) (*EnumerateProfilesResponse, error) {
+	tmpFile, err := os.CreateTemp("", "enumprofile")
+	if err != nil {
+		return nil, err
+	}
+	tmpFile.Close()
+	defer os.Remove(tmpFile.Name())
+
+	cmd := exec.Command(cl.exePath, "--format=json", "--enumprofles", profileFolder, tmpFile.Name())
+
+	if cmd.ProcessState.ExitCode() != 0 {
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, NewParsedError(cmd.ProcessState.ExitCode(), out)
+	}
+
+	tmpFile, err = os.Open(tmpFile.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	var resp EnumerateProfilesResponse
+	if err = json.NewDecoder(tmpFile).Decode(&resp); err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+type ParsedError struct {
+	b []byte
+	s string
+
+	ProcessExitCode int
+	Code            int64
+	Message         string
+}
+
+func (p *ParsedError) Error() string {
+
+	return ""
+}
+
+func NewParsedError(exitCode int, output []byte) *ParsedError {
+	pe := &ParsedError{
+		ProcessExitCode: exitCode,
+		b:               output,
+		s:               string(output),
+	}
+
+	parsed, err := ParseOutput(string(output))
+	if err != nil {
+		fmt.Println(pe)
+		return pe
+	}
+
+	for _, line := range parsed.Lines {
+		switch line.Type() {
+		case ErrorLine:
+			l := line.(CmdOutputErrorLine)
+
+			pe.Code = l.Code
+			pe.Message = l.Message
+		}
+	}
+
+	return pe
+}
+
+type CmdOutput struct {
+	Lines    []CmdOutputLine
+	Duration time.Duration
+}
+
+func ParseOutput(s string) (CmdOutput, error) {
+	lines := strings.Split(s, "\n")
+	var outLines []CmdOutputLine
+	var cmdOutput CmdOutput
+
+	for _, line := range lines {
+		items := strings.Split(line, "\t")
+		fmt.Println(strings.Join(items, "|"), len(items))
+
+		if len(line) == 0 {
+			continue
+		}
+
+		switch items[0] {
+		case "Error", "Errors":
+			var l CmdOutputErrorLine
+
+			code, err := strconv.ParseInt(items[1], 10, 64)
+			if err != nil {
+				fmt.Println(err)
+			}
+			l.Code = code
+			l.Message = items[2]
+
+			outLines = append(outLines, l)
+		case "Duration":
+			var l CmdOutputDurationLine
+
+			parsed, err := time.Parse("04:05", items[1])
+			if err != nil {
+				fmt.Println(err)
+			}
+			l.dur = time.Duration(parsed.Minute())*time.Minute + time.Duration(parsed.Second())*time.Second
+			cmdOutput.Duration = l.dur
+
+			outLines = append(outLines, l)
+		default:
+			var l CmdOutputIdentityLine
+			l.Line = line
+			l.Parts = items
+
+			outLines = append(outLines, l)
+		}
+	}
+
+	cmdOutput.Lines = outLines
+
+	return cmdOutput, nil
 }
---
+++ types.go
@@ -0,0 +1,90 @@
+package pdftoolbox
+
+import "time"
+
+type LineOutputType uint8
+
+const (
+	IdentityLine LineOutputType = iota
+	ErrorLine
+	DurationLine
+)
+
+type CmdOutputLine interface {
+	Type() LineOutputType
+	String() string
+}
+
+type CmdOutputIdentityLine struct {
+	Line  string
+	Parts []string
+}
+
+func (l CmdOutputIdentityLine) Type() LineOutputType {
+	return IdentityLine
+}
+
+func (l CmdOutputIdentityLine) String() string {
+	return l.Line
+}
+
+type CmdOutputDurationLine struct {
+	Line  string
+	Parts []string
+	dur   time.Duration
+}
+
+func (l CmdOutputDurationLine) Type() LineOutputType {
+	return DurationLine
+}
+
+func (l CmdOutputDurationLine) String() string {
+	return l.Line
+}
+
+func (l CmdOutputDurationLine) Duration() time.Duration {
+	return l.dur
+}
+
+type CmdOutputErrorLine struct {
+	Code    int64
+	Message string
+}
+
+func (l CmdOutputErrorLine) Type() LineOutputType {
+	return ErrorLine
+}
+
+func (l CmdOutputErrorLine) String() string {
+	return l.Message
+}
+
+type EnumerateProfilesResponse struct {
+	Information Information `json:"information"`
+	Profiles    []Profiles  `json:"profiles"`
+}
+type Information struct {
+	Computername    string    `json:"computername"`
+	DateTime        time.Time `json:"date_time"`
+	OperatingSystem string    `json:"operating_system"`
+	ProductName     string    `json:"product_name"`
+	ProductVersion  string    `json:"product_version"`
+	Username        string    `json:"username"`
+}
+type Variables struct {
+	Key   string `json:"key"`
+	Label string `json:"label"`
+	Type  string `json:"type"`
+	Value any    `json:"value"`
+}
+
+type Profiles struct {
+	Comment          string         `json:"comment"`
+	CreationDate     time.Time      `json:"creation_date"`
+	ModificationDate time.Time      `json:"modification_date"`
+	Name             string         `json:"name"`
+	Path             string         `json:"path"`
+	Size             string         `json:"size"`
+	Variables        []Variables    `json:"variables"`
+	Vars             map[string]any `json:"vars"`
+}
